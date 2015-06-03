7100c13aa7a0061482d072aa92672c21e4ca7ff2
ISMAELMARTINEZ <ISMAELMARTINEZ@GMAIL.COM>
2015-06-03 17:09:04 +0200

    adding play and stop bars

--- html/webaudio.js
+++ html/webaudio.js
@@ -13,3 +13,19 @@ function setFrequency(d) {
 function resetFrequency() {
   oscillator.frequency.value = 0;
 }
+
+var timeouts = [];
+function playBars() {
+	d3.selectAll("rect").each(
+    function (d, i) {
+      timeouts.push(setTimeout(function () { setFrequency(d)}, 500* i));
+    });
+}
+
+function stopPlayBars() {
+  timeouts.forEach(
+    function (timeout) {
+      clearTimeout(timeout);
+      resetFrequency();
+    });
+}
